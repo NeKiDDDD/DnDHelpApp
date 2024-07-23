a6159ca506e198c6c67bfae0916096478f005ee0
lax3331 <la2life1234@gmail.com>
2024-07-23 17:41:41 +0400

    Initial Commit

---
+++ DnDHelpApp/DnDHelpAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DnDHelpAppApp.swift
+//  DnDHelpApp
+//
+//  Created by Nikita Kuznetsov on 23.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct DnDHelpAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
